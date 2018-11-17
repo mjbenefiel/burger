b341c0ec08b2b6dbb46b3d3176915f2fa9d9e1a0
mjbenefiel <michaeljbenefiel@gmail.com>
2018-11-17 09:58:39 -0600

    11172018 commits heroku stuff

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,17 @@
 var mysql = require("mysql");
 require("dotenv").config();
 
-var connection = mysql.createConnection({
+var connection = mysql.createConnection(process.env.JAWSDB_URL);
+
+
+
+/*mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "mjbenefiel",
   password: "dur@cell15",
   database: "burgers_db"
-});
+}); */
 
 // Make connection.
 connection.connect(function(err) {
